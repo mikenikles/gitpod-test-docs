32d3f2bcec7e080aca97f8fe804ec4f21b1ba761
Mike Nikles <mikenikles@pm.me>
2021-12-16 23:16:27 +0000

    Add docs.

---
+++ docs/__layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Menu from "$lib/docs/menu.svelte";
+  import MobileMenu from "$lib//docs/mobile-menu/index.svelte";
+  import Search from "$lib/docs/search.svelte";
+  import "../src/assets/markdown-commons.scss";
+  import { MENU } from "./menu";
+</script>
+
+<div class="pb-10 md:flex md:pt-10">
+  <div class="hidden md:block md:w-1/3 md:pt-24 md:pr-8">
+    <Menu {MENU} />
+  </div>
+  <div class="md:w-2/3">
+    <Search />
+    <MobileMenu {MENU} />
+    <slot />
+  </div>
+</div>
